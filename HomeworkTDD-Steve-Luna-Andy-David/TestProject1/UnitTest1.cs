0ac068a4fa38ec5ec201ef8480e788302ac7ef54
david.he <david.he@onelab.tw>
2025-07-09 16:08:42 +0400

    refac: Extract method for Assertment

--- HomeworkTDD-Steve-Luna-Andy-David/TestProject1/UnitTest1.cs
+++ HomeworkTDD-Steve-Luna-Andy-David/TestProject1/UnitTest1.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using FluentAssertions.Primitives;
 using HomeworkTDD_Steve_Luna_Andy_David.Controllers;
 using HomeworkTDD_Steve_Luna_Andy_David.Enums;
 using HomeworkTDD_Steve_Luna_Andy_David.Models;
@@ -24,8 +25,19 @@ public class Tests
         GivenMatchScore("");
         var matchController = new MatchController(_matchRepo);
         var result = matchController.UpdateMatchScores(MatchId, MatchEvent.HomeGoal);
-        _matchRepo.Received(1).UpdateMatchScores(Arg.Is<Match>(x => x.MatchId == MatchId && x.MatchScore.Score == "H"));
-        result.Should().Be("1:0 First Half");
+        ShouldUpdateScore("H");
+        DisplayScoreMustBe(result, "1:0 First Half");
+    }
+
+    private static AndConstraint<StringAssertions> DisplayScoreMustBe(string result, string displayScore)
+    {
+        return result.Should().Be(displayScore);
+    }
+
+    private void ShouldUpdateScore(string score)
+    {
+        _matchRepo.Received(1)
+            .UpdateMatchScores(Arg.Is<Match>(x => x.MatchId == MatchId && x.MatchScore.Score == score));
     }
 
     private void GivenMatchScore(string score)
